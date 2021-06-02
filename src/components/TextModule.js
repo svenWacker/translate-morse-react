da0295cea8aee0539b745f7e5302d3696e564954
svenWacker <s.u.wacker@outlook.de>
2021-06-02 10:01:25 +0200

    morse solved, without styling

--- src/components/TextModule.js
+++ src/components/TextModule.js
@@ -1,77 +1,90 @@
 import React, { useState } from "react";
 
-const morseTable = {
-  "-----": "0",
-  ".----": "1",
-  "..---": "2",
-  "...--": "3",
-  "....-": "4",
-  ".....": "5",
-  "-....": "6",
-  "--...": "7",
-  "---..": "8",
-  "----.": "9",
-  ".-": "a",
-  "-...": "b",
-  "-.-.": "c",
-  "-..": "d",
-  ".": "e",
-  "..-.": "f",
-  "--.": "g",
-  "....": "h",
-  "..": "i",
-  ".---": "j",
-  "-.-": "k",
-  ".-..": "l",
-  "--": "m",
-  "-.": "n",
-  "---": "o",
-  ".--.": "p",
-  "--.-": "q",
-  ".-.": "r",
-  "...": "s",
-  "-": "t",
-  "..-": "u",
-  "...-": "v",
-  ".--": "w",
-  "-..-": "x",
-  "-.--": "y",
-  "--..": "z",
-  ".-.-.-": ".",
-  "--..--": ",",
-  "..--..": "?",
-  "-.-.--": "!",
-  "-....-": "-",
-  "-..-.": "/",
-  ".--.-.": "@",
-  "-.--.": "(",
-  "-.--.-": ")",
-};
-
 function TextModule() {
-  const [input, setInput] = useState("");
-
-  const handleChange = (e) => {
-    e.preventDefault();
+  const morseTable = {
+    0: "-----",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+    a: ".-",
+    b: "-...",
+    c: "-.-.",
+    d: "-..",
+    e: ".",
+    f: "..-.",
+    g: "--.",
+    h: "....",
+    i: "..",
+    j: ".---",
+    k: "-.-",
+    l: ".-..",
+    m: "--",
+    n: "-.",
+    o: "---",
+    p: ".--.",
+    q: "--.-",
+    r: ".-.",
+    s: "...",
+    t: "-",
+    u: "..-",
+    v: "...-",
+    w: ".--",
+    x: "-..-",
+    y: "-.--",
+    z: "--..",
+    ".": "......",
+    ",": ".-.-.-",
+    ";": "-.-.-.",
+    ":": "---...",
+    "?": "..--..",
+    "!": "--..--",
+    "(": "-.--.",
+    ")": "-.--.-",
+    "/": "-..-.",
+    "-": "-....-",
+    "@": ".--.-.",
+    "=": "-...-",
+    "+": ".-.-.",
+    $: "...-..-",
+    "'": ".----.",
+    "&": ".-...",
+    " ": "/",
   };
+  const [inputText, setInput] = useState("");
+  const [result, setOutput] = useState("");
 
-  const AlphaKeys = () => Object.keys(morseTable);
+  function changeHandle(e) {
+    setInput(e.target.value);
+  }
 
-  const MorseKeys = () =>
-    AlphaKeys().map(function (alphakey) {
-      return morseTable[alphakey];
-    });
-  console.log(Object.entries(morseTable));
+  function textToMorse() {
+    let result = "";
+    let morseArray = inputText.toString().split("");
+
+    for (let i = 0; i < morseArray.length; i++) {
+      result += morseTable[morseArray[i].toLowerCase()];
+    }
+    setOutput(result);
+  }
 
   return (
-    <div>
-      <form onSubmit={handleChange}>
-        <input type="text" placeholder="Please, enter your text here"></input>
-        <button className="submit-button">Translate</button>
-      </form>
-      <div className="result" placeholder="Result">
-        {MorseKeys}
-      </div>
+    <div className="container">
+      <h1>Morse Code Translator</h1>
+      <input
+        type="text"
+        className="form-control"
+        value={inputText}
+        onChange={changeHandle}
+        onKeyUp={textToMorse}
+      />
+      <div>Text: {inputText}</div>
+      <div>Morse: {result}</div>
     </div>
   );
 }
